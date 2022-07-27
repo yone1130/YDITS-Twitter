57975286632be233d934599b075fd63a8988c9e1
よね/Yone <66835584+yone1130@users.noreply.github.com>
2022-07-27 23:06:32 +0900

    Update YDITS-Twitter.bat

--- Windows/YDITS-Twitter.bat
+++ Windows/YDITS-Twitter.bat
@@ -1,4 +1,5 @@
-@REM YDITS-Twitter.bat / YDITS for Twitter with Windows ver 1.2.6 / Yone1130
+@REM YDITS-Twitter.bat | YDITS for Twitter with Windows ver 1.2.6
+@REM (c) 2022 よね/Yone
 
 @echo off
 python Codes\main.py
